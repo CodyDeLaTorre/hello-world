5ddc5ee37bb58971d76e918e9670315e39054bf7
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-09-21 16:21:43 -0700

    Lab 6

---
+++ app.js
@@ -0,0 +1,7 @@
+function signUp(){
+    let userEmail = prompt("What is your email?")
+};
+
+function contactUs(){
+    alert("I don't want to talk")
+};
---
+++ message.js
@@ -0,0 +1,11 @@
+
+
+let userName = prompt("Welcome to my news page! What is your name?")
+
+if(userName === "Kassie"){
+    alert("Welcome teach!")
+} else if (userName === "Kai"){
+    alert("oh it's you...")
+} else {
+    alert("Enjoy the news :)")
+}
