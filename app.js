2a53ddb2ed5e6de1a9001bb28fee7004b5b8e77d
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-09-22 12:07:20 -0700

    lab-7

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+'use strict';
+
+let userName = prompt("Welcome to my news page! What is your name?")
+
+
+if(userName === "Kassie" || userName === "kassie" ){
+    alert("Welcome teach!")
+} else if (userName == "Cody" || userName == "cody"){
+    alert("Welcome, creator.")
+}else if (userName === "Kai" || userName === "kai"){
+    alert("oh it's you...")
+} else if (userName == ""){
+    prompt("Please enter you name!")
+    if(userName == ""){
+        alert("I didn't want to know you anyway")
+    }
+} else {
+    alert("Enjoy the news :) " + userName)
+}
+
+
+let userEmail;
+
 function signUp(){
-    let userEmail = prompt("What is your email?")
+    let userEmail = prompt("What is your email?");
+    return userEmail;
 };
+
+
+
 
 function contactUs(){
     alert("I don't want to talk")
 };
+
+
